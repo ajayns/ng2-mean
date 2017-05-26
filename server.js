777548e5d777905d005e55d9715c29eec64babea
Ajay NS <ajay.ns@outlook.com>
2017-05-26 20:36:33 +0530

    Mod comments

--- server.js
+++ server.js
@@ -36,18 +36,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
+// Get a port from Environment and store in express
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
+// Create http server
 const server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+// Listen on provided port
 server.listen(port, () => console.log(`API running on localhost:${port}`));
