830a7941b79c78da2b5a48b662b3b205d6de7eec
Ajay NS <ajay.ns@outlook.com>
2017-05-26 14:42:46 +0530

    Add pages and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,46 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
 
 // import { PostsComponent } from './posts/posts.component'; // component
 // import { PostsService } from './posts.service'; // provider service
 
-/* Define routes
+// Define routes
 const routes = [
     {
         path: '',
-        redirectTo: 'posts',
+        redirectTo: 'home',
         pathMatch: 'full'
     },
     {
-        path: 'posts',
-        component: PostsComponent
+        path: 'home',
+        component: HomepageComponent
+    },
+    {
+        path: 'about',
+        component: AboutpageComponent
     }
 ];
-*/
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    JumbotronComponent,
+    NavbarComponent,
+    HomepageComponent,
+    AboutpageComponent,
     // PostsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    // RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes)
   ],
   providers: [], // [PostsService],
   bootstrap: [AppComponent]
